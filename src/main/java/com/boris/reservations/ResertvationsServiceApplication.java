c6bc0dbe8c94e0aaaa4350824fabc98de3ad5d62
BorislavShekerov <shekerovborislav@gmail.com>
2017-01-11 20:47:52 +0000

    Project Boilerplate

---
+++ src/main/java/com/boris/reservations/ResertvationsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.boris.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResertvationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResertvationsServiceApplication.class, args);
+	}
+}
